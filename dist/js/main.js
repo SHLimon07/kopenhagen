aa8f2ceb3ef68741e45cbe754cdbce671c702e44
SHLimon07 <sajjadblanco07@gmail.com>
2020-07-23 22:37:24 +0600

    Menu bar created

---
+++ dist/js/main.js
@@ -0,0 +1,8 @@
+
+function navBarToggle() {
+    var icon = document.querySelector('.buttonDiv');
+    var menu = document.querySelector('.collaps');
+
+    icon.classList.toggle('toggle');
+    menu.classList.toggle('toggle');
+}
